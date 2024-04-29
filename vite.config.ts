c207c23289f9f07004d906ac3c037a02f308250a
sebastiannarvaez23 <narvaezsebas8@gmail.com>
2024-04-28 23:01:54 -0500

    Modificacion de la ruta base

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://sebastiannarvaez23.gitlab.io/dv-idico-web'
+  base: 'https://sebastiannarvaez23.github.io/dv-idico-web/'
 })
