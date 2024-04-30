020a87a9c94f5d2fe13ef5134f196a387458abb0
sebastiannarvaez23 <narvaezsebas8@gmail.com>
2024-04-29 19:41:21 -0500

    Modificando el navigate

--- src/main.tsx
+++ src/main.tsx
@@ -1,24 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { RouterProvider, createHashRouter, RouteObject } from 'react-router-dom';
+import { useNavigate, RouterProvider, createHashRouter, RouteObject } from 'react-router-dom';
 import HomePage from './pages/HomePage.tsx';
 import AuthPage from './pages/AuthPage.tsx';
 import './index.css';
 
 const isAuthenticated = !!localStorage.getItem('token');
 
-const PrivateRoute: React.FC<{ auth: React.ReactNode, element: React.ReactNode }> = ({ auth, element }) => {
+const PrivateRoute: React.FC<{ element: React.ReactNode }> = ({ element }) => {
+  const navigate = useNavigate();
   if (isAuthenticated) {
+    navigate('/');
     return <>{element}</>;
   } else {
-    return <>{auth}</>;
+    navigate('/auth');
+    return null;
   }
 };
 
 const routes: RouteObject[] = [
   {
     path: '/',
-    element: <PrivateRoute auth={<AuthPage />} element={<HomePage />} />
+    element: <PrivateRoute element={<HomePage />} />
   },
   {
     path: '/auth',
